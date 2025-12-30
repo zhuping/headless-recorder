d8147d37576d7a91b2f7f58b18ce982457fa690d
无问 <zhuping@cai-inc.com>
2025-12-30 17:48:15 +0800

    refactor(build): update babel config and remove vue config
    Update babel configuration to add optional chaining and nullish coalescing plugins
    Remove vue.config.js as its configuration is now handled in package.json

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   presets: ['@vue/cli-plugin-babel/preset'],
+  plugins: [
+    '@babel/plugin-transform-optional-chaining',
+    '@babel/plugin-transform-nullish-coalescing-operator',
+  ],
 }
